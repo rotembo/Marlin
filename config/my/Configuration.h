734994af4fe12b69dd4a25e7d09a2516b93f48d1
rotembo <rotembuhbut@gmail.com>
2020-05-01 03:18:27 +0300

    update

--- config/my/Configuration.h
+++ config/my/Configuration.h
@@ -1319,7 +1319,7 @@
   #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
   #define LEVEL_CORNERS_HEIGHT      0.0   // (mm) Z height of nozzle at leveling points
   #define LEVEL_CORNERS_Z_HOP       4.0   // (mm) Z height of nozzle between leveling points
-  //#define LEVEL_CENTER_TOO              // Move to the center after the last corner
+  #define LEVEL_CENTER_TOO              // Move to the center after the last corner
 #endif
 
 /**
